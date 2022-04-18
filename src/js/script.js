c39cdb159fbb0ef32be7d5a8c008133b6a85865d
Max <max11cz@seznam.cz>
2022-04-18 23:06:35 +0200

    add catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-const numb = 42;
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 800,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+            }}
+        ]
+    });
+});
