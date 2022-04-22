c03a45b0b654a64f18ba0ebeb15526c0477cd335
Max <max11cz@seznam.cz>
2022-04-22 13:52:24 +0200

    little fixes script send form

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('', 'Pulsemeter');   
-$mail->addAddress('cihiyo2692@topyte.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
